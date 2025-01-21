0c36c7db75dc424e84b2f7094b5d2e92884c1087
Salim Hossain <salimhossain@gmail.com>
2025-01-21 11:03:19 +0600

    init

---
+++ background.js
@@ -0,0 +1,18 @@
+// background.js
+let lastTabId = null;
+let currentTabId = null;
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  lastTabId = currentTabId;
+  currentTabId = activeInfo.tabId;
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "switch-last-tab" && lastTabId !== null) {
+    chrome.tabs.update(lastTabId, { active: true }, () => {
+      if (chrome.runtime.lastError) {
+        console.error("Error switching tabs:", chrome.runtime.lastError);
+      }
+    });
+  }
+});
